565f8330ebf99bad8c8fe9e88cf1e9be30950c86
bobi.rival <bahmadrival@gmail.com>
2024-11-12 11:32:22 +0700

    Fixing error cors

--- src/main/java/com/grasia/prima/ppi/api/config/CorsConfig.java
+++ src/main/java/com/grasia/prima/ppi/api/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
